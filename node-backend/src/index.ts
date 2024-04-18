9c02a77c48fed59a33e3abe7edcdb1830866e101
Aditya Raj <adistrim.dev@gmail.com>
2024-04-19 00:21:38 +0530

    backend v1.0

---
+++ node-backend/src/index.ts
@@ -0,0 +1,17 @@
+// simple websocket code from https://github.com/websockets/ws
+
+import { WebSocketServer } from 'ws';
+import { GameManager } from './GameManager';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const gameManager = new GameManager();
+
+
+wss.on('connection', function connection(ws) {
+
+    gameManager.addUser(ws);
+    ws.on("disconnect", () => gameManager.removeUser(ws));
+
+});
+
