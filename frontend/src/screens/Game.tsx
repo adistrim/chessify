399bac0f8deb6bb1484f6a0dfd6c43d38637decd
Aditya Raj <adistrim.dev@gmail.com>
2024-04-19 15:00:15 +0530

    fix deployment bug 1

--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -13,7 +13,7 @@ export const GAME_OVER = "game_over";
 export const Game = () => {
 
     const socket = useSocket();
-    const [chess, setChess] = useState(new Chess())
+    const [chess] = useState(new Chess())
     const [board, setBoard] = useState(chess.board());
     const [started, setStarted] = useState(false);
 
@@ -43,7 +43,7 @@ export const Game = () => {
         return () => {
             socket.onmessage = null;
         }
-    }, [socket]);
+    }, [chess, socket]);
 
     if (!socket) {
         return <div className="flex justify-center">
