d9ad13362d20e7a412d40ac5e69070ecea6031b3
Aditya Raj <adistrim.dev@gmail.com>
2025-05-12 01:03:17 +0530

    added message data

---
+++ frontend/src/data/messages.ts
@@ -0,0 +1,11 @@
+export const chessLoadingMessages = [
+  "Did you know? The number of possible unique chess games is greater than the number of atoms in the universe!",
+  "The longest official chess game lasted 269 moves and ended in a draw.",
+  "The word 'Checkmate' comes from the Persian phrase 'Shah Mat' which means 'the king is dead'.",
+  "The first chess computer program was developed at MIT in 1957.",
+  "There are 318,979,564,000 possible ways to play just the first four moves in chess.",
+  "Moving the pieces... not literally though, that would be creepy.",
+  "Arranging the knights and bishops for battle...",
+  "Polishing the royal crowns for the king and queen...",
+  "Teaching pawns the art of patience...",
+];
